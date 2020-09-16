d972c692de12d6bd1ceffa69aa643365992bb046
f-westergren <folke.westergren@gmail.com>
2020-09-16 13:08:55 -0400

    add update route

--- routes/users.js
+++ routes/users.js
@@ -15,13 +15,31 @@ router.get('/:id', async (req, res, next) => {
 })
 
 router.post('/', async (req, res, next) => {
-  // Add validation.
+  // TODO: Add validation.
   try {
     const newUser = await User.register(req.body);
     const token = createToken(newUser);
     return res.status(201).json({ token });
   } catch (e) {
     return next(e)
+  }
+})
+
+router.patch('/:id', async (req, res, next) => {
+  // How to prevent users from updating their email?
+  try {
+    await User.authenticate({
+      id: req.params.id,
+      password: req.body.password
+    }); 
+
+    delete req.body.password;
+    // TODO: Add validation
+    const user = await User.update(req.params.id, req.body);
+
+    return res.json({ user })
+  } catch (err) {
+    return next(err);
   }
 })
 module.exports = router;
