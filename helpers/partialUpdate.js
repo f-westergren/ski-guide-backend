76204be028822c3bd4d45e1ef6f5f6e5d43afb98
f-westergren <folke.westergren@gmail.com>
2020-09-16 13:08:32 -0400

    add helper function for patch request

---
+++ helpers/partialUpdate.js
@@ -0,0 +1,26 @@
+function sqlForPartialUpdate(table, items, key, id) {
+  let idx = 1;
+  let columns = [];
+
+  for (let key in items) {
+    if (key.startsWith('_')) {
+      delete items[key]
+    }
+  }
+
+  for (let column in items) {
+    columns.push(`${column}=$${idx}`);
+    idx += 1;
+  }
+
+  let cols = columns.join(', ');
+  let query = `UPDATE ${table} SET ${cols} WHERE ${key}=$${idx} RETURNING *`;
+
+  let values = Object.values(items);
+  values.push(id)
+  console.log(query, values);
+  return {query, values};
+}
+
+module.exports = sqlForPartialUpdate;
+
