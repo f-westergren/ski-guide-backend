88c5cde75d86f6605c35a2074b2b8f7a9e45fc60
f-westergren <folke.westergren@gmail.com>
2020-09-21 07:47:43 -0400

    update names and add tables

--- data.sql
+++ data.sql
@@ -1,53 +1,55 @@
 DROP TABLE IF EXISTS favorites;
 DROP TABLE IF EXISTS messages;
 DROP TABLE IF EXISTS reviews;
-DROP TABLE IF EXISTS guide_profile;
-DROP TABLE IF EXISTS user_profile;
+DROP TABLE IF EXISTS guide_profiles;
+DROP TABLE IF EXISTS user_profiles;
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
-  email TEXT NOT NULL UNIQUE,
-  password TEXT NOT NULL
+  email text NOT NULL UNIQUE,
+  password text NOT NULL
 );
 
-CREATE TABLE user_profile (
-  id INTEGER PRIMARY KEY REFERENCES users(id),
-  first_name TEXT NOT NULL,
-  last_name TEXT NOT NULL,
-  skill_level TEXT NOT NULL,
-  location TEXT,
-  image_url TEXT
+CREATE TABLE user_profiles (
+  id integer PRIMARY KEY REFERENCES users(id),
+  first_name text NOT NULL,
+  last_name text NOT NULL,
+  skill_level text NOT NULL,
+  is_guide boolean DEFAULT FALSE,
+  location text,
+  image_url text
 );
 
-CREATE TABLE guide_profile (
-  id INTEGER PRIMARY KEY REFERENCES users(id),
-  coordinates TEXT NOT NULL,
-  bio TEXT NOT NULL,
-  type TEXT NOT NULL
+CREATE TABLE guide_profiles (
+  id integer PRIMARY KEY REFERENCES users(id),
+  coordinates text NOT NULL,
+  bio text NOT NULL,
+  type text ARRAY
 );
 
 CREATE TABLE reviews (
   id SERIAL PRIMARY KEY,
-  by_user_id INTEGER REFERENCES users(id),
-  about_user_id INTEGER REFERENCES users(id),
-  text TEXT NOT NULL,
-  rating INTEGER NOT NULL,
-  CHECK (rating > 0 AND rating < 6)
+  by_user_id integer REFERENCES users(id),
+  of_user_id integer REFERENCES users(id),
+  content text NOT NULL,
+  time_stamp timestamp NOT NULL,
+  rating integer NOT NULL,
+  CHECK (rating >= 1 AND rating <= 5)
 );
 
 CREATE TABLE messages (
   id SERIAL PRIMARY KEY,
-  from_user_id INTEGER REFERENCES users(id),
-  to_user_id INTEGER REFERENCES users(id),
-  text TEXT NOT NULL,
+  from_user_id integer REFERENCES users(id),
+  to_user_id integer REFERENCES users(id),
+  content text NOT NULL,
   time_stamp TIMESTAMP NOT NULL
 );
 
 CREATE TABLE favorites (
   id SERIAL PRIMARY KEY,
-  guide_id INTEGER REFERENCES users(id),
-  user_id INTEGER REFERENCES users(id)
+  guide_id integer REFERENCES users(id),
+  user_id integer REFERENCES users(id)
 );
 
 INSERT INTO users
@@ -55,19 +57,34 @@ INSERT INTO users
 VALUES
   ('testuser@gmail.com', 'password'),
   ('testuse2r@gmail.com', 'password'),
-  ('testuser3@gmail.com', 'password');
+  ('testuser3@gmail.com', 'password'),
+  ('cat@stevens.com', 'wildworld');
 
-INSERT INTO user_profile 
-  (id, first_name, last_name, skill_level)
+INSERT INTO user_profiles 
+  (id, first_name, last_name, skill_level, is_guide)
 VALUES
-  (1, 'Nils', 'Nilsson', 'beginner'),
-  (2, 'Anders', 'Andersson', 'intermediate'),
-  (3, 'Bosse', 'Bosson', 'advanced');
+  (1, 'Nils', 'Nilsson', 'beginner', 'false'),
+  (2, 'Anders', 'Andersson', 'intermediate', 'false'),
+  (3, 'Bosse', 'Bosson', 'advanced', 'true'),
+  (4, 'Cat', 'Stevens', 'pro', 'true');
+
+INSERT INTO guide_profiles
+  (id, coordinates, bio, type)
+VALUES
+  (3, '1-2', 'I am a cool cat!', '{"ski", "telemark"}'),
+  (4, '1-2', 'I am a cooler cat!', '{"snowboard"}');
 
 INSERT INTO messages
-  (from_user_id, to_user_id, text, time_stamp)
+  (from_user_id, to_user_id, content, time_stamp)
 VALUES
   (1, 2, 'Hi there wanna ski?!', '2020-09-19'),
   (3, 1, 'Hi there wanna snowboard?!', '2020-09-17'),
   (2, 3, 'Are you available to get some pow!?', '2020-09-12');
+
+INSERT INTO reviews
+  (by_user_id, of_user_id, content, time_stamp, rating)
+VALUES
+  (1, 3, 'What a great guide, loved it!', '2020-01-03', 5),
+  (2, 4, 'What a crappy guide, hated him!', '2020-03-03', 1),
+  (2, 3, 'Bosse is the best, always book him!', '2020-03-03', 4);
 
